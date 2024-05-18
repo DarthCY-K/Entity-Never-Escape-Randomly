3a61aea18bfe76526229bac7678097eadc51969d
DarthCY <452710557@qq.com>
2024-05-18 21:13:36 +0800

    Initial commit

---
+++ Source/EntityNeverEscapeRandomly/Patch/PatchMain.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using Verse;
+
+namespace EntityNeverEscapeRandomly.Patch
+{
+    [StaticConstructorOnStartup]
+    public class PatchMain
+    {
+        public static Harmony instance;
+
+        static PatchMain()
+        {
+            instance = new Harmony("EntityNeverEscapeRandomly.Patch");
+            instance.PatchAll(Assembly.GetExecutingAssembly());
+
+            Log.Message("EntityNeverEscapeRandomly Patched");
+        }
+    }
+}
---
+++ Source/EntityNeverEscapeRandomly/Patch/Patch_CompHoldingPlatformTarget.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace EntityNeverEscapeRandomly.Patch
+{
+    [HarmonyPatch(typeof(CompHoldingPlatformTarget), "CaptivityTick")]
+    public class Patch_CompHoldingPlatformTarget_CaptivityTick
+    {
+        static bool Prefix(Pawn pawn, CompHoldingPlatformTarget __instance)
+        {
+            pawn.mindState.entityTicksInCaptivity++;
+            if (__instance.targetHolder is Building_HoldingPlatform building_HoldingPlatform && building_HoldingPlatform != __instance.HeldPlatform && building_HoldingPlatform.Occupied)
+            {
+                __instance.targetHolder = null;
+            }
+
+            //if (__instance.parent.IsHashIntervalTick(2500))
+            //{
+            //    float num = ContainmentUtility.InitiateEscapeMtbDays(pawn);
+            //    if (num >= 0f && Rand.MTBEventOccurs(num, 60000f, 2500f))
+            //    {
+            //        __instance.Escape(initiator: true);
+            //    }
+            //}
+
+            return false;
+        }
+    }
+}
